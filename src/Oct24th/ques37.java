05c2833ee7441195b1ad78a05ddc7e228196b3ed
Nupur Gupta <nupurg1905@gmail.com>
2023-10-26 14:42:45 -0400

    commit

---
+++ src/Oct24th/ques37.java
@@ -0,0 +1,16 @@
+package Oct24th;
+
+public class ques37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 1;
+        
+        do {
+            System.out.println(count);
+            count++;
+        } while (count <= 10);
+
+	}
+
+}
